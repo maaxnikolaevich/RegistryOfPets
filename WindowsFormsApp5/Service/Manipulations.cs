43251c938fdcd138d29875b3e5736ae2770c0a27
Maks <lebedkom03@gmail.com>
2021-12-11 05:52:15 +0500

    AddMethods

---
+++ WindowsFormsApp5/Service/Manipulations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistryOfPets.Service
+{
+    class Manipulations
+    {
+        public void Add(int id, string categoryPet,  string gender,  string dateBirth,  int idmark, int idChip, string namePet, string nameOwner)
+        { 
+
+        }
+
+        public void Edit(int id, string categoryPet, string gender, string dateBirth, int idmark, int idChip, string namePet, string nameOwner)
+        {
+
+        }
+
+        public void AddPhoto(int idPhoto, string storage)
+        {
+
+        }
+
+        public void DeletePhoto(int idPhoto)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp5/Service/ViewServiceOfOwners.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistryOfPets.Controller
+{
+    class ViewServiceOfOwners
+    {
+        public void GetOwner(int idField)
+        {
+
+        }
+
+        public void Edit(int idField)
+        {
+
+        }
+
+        public void AddPhoto(int idField , string name, string address, string countryg, string phone, string email, int countPets, int countCats, int countDogs)
+        {
+
+        }
+
+        public void DeletePhoto(int idField, string interprice, string inn, string kpp, string address, string country, string phone, string email, int countPets, int countCats, int countDogs)
+        {
+
+        }
+    }
+}
